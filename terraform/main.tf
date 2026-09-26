b3a733c2a74fef1d8f5b61ef20c0b5faf6eba883
vijay kashyap <vijay.kashyap@oracle.com>
2026-09-26 14:38:21 +0530

    Configure EKS access

--- terraform/main.tf
+++ terraform/main.tf
@@ -152,7 +152,8 @@ resource "aws_eks_cluster" "main" {
   }
 
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   depends_on = [
